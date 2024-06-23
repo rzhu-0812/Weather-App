49475f0d64cd50722526bce3adab85050996ba5c
rzhu-0812 <86477098+rzhu-0812@users.noreply.github.com>
2024-06-22 21:28:48 -0400

    Basic console based version of final product
    User inputs city name, and gets info back on city weather. Exceptions created for request errors and 404 error.

---
+++ weather_app.py
@@ -0,0 +1,36 @@
+import requests
+
+api_key = "API KEY"
+
+if api_key is None:
+    raise ValueError("No API key found in environment variables")
+
+user_input = input("Enter city: ")
+
+try:
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+    weather_data.raise_for_status()
+    data = weather_data.json()
+
+    if data['cod'] == '404':
+        print("No City Found")
+    else:
+        weather = data['weather'][0]['main']
+        description = data['weather'][0]['description']
+        temp = round(data['main']['temp'])
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+
+        print(f"Weather in {user_input}: {weather} ({description})")
+        print(f"Temperature: {temp}ºF")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} mph")
+except requests.exceptions.HTTPError as err:
+    print(f"HTTP error occurred: {err}")
+except requests.exceptions.ConnectionError as err:
+    print(f"Error connecting: {err}")
+except requests.exceptions.Timeout as err:
+    print(f"Timeout error: {err}")
+except requests.exceptions.RequestException as err:
+    print(f"An error occurred: {err}")
